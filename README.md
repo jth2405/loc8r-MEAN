8cd8741d38da29412b87a31862218963f0aa4d3e
Tae-Hwan Jung <jth0880@gmail.com>
2021-11-28 20:54:50 +0900

    #20

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Loc8r-MEANproj" 
+"# Loc8r-MEANproj" 
+"# loc8r-MEAN" 
---
+++ app_public/src/app/location-details/location-details.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Location, Review } from '../location';
+import { Loc8rDataService } from '../loc8r-data.service';
+import { ApiKey } from '../api-key';
+
+@Component({
+  selector: 'app-location-details',
+  templateUrl: './location-details.component.html',
+  styleUrls: ['./location-details.component.css']
+})
+
+export class LocationDetailsComponent implements OnInit {
+
+  @Input() location: Location;
+
+  public googleAPIKey=ApiKey.API_KEY;
+
+ 
+
+  public formVisible: boolean = false;
+
+  constructor(private loc8rDataService: Loc8rDataService) { }
+
+  ngOnInit() {
+  }
+
+  public formError: string;
+
+  public newReview: Review={
+    author: '',
+    rating: 5,
+    reviewText: ''
+  };
+
+  private formIsValid(): boolean {
+    if (this.newReview.author && this.newReview.rating
+    && this.newReview.reviewText) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  private resetAndHideReviewForm(): void {
+    this.formVisible = false;
+    this.newReview.author = '';
+    this.newReview.rating = 5;
+    this.newReview.reviewText = '';
+  }
+  
+  public onReviewSubmit():void {
+    this.formError = '';
+    if (this.formIsValid()) {
+      console.log(this.newReview);
+      this.loc8rDataService.addReviewByLocationId(this.location._id,this.newReview)
+       .then((review: Review) => {
+         let reviews = this.location.reviews.slice(0);
+         reviews.unshift(review);
+         this.location.reviews = reviews;
+         this.resetAndHideReviewForm();
+      });
+    } else {
+      this.formError = 'All fields required, please try again';
+    }
+  }
+  
+  
+  
+}
+
